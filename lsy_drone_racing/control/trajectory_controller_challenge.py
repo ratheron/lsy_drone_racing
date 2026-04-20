1934ec0346f2a2eb1dc6efec93da5be3bd5a2085
Rather1337 <marcel.rath@gmx.de>
2026-04-20 18:47:10 +0200

    Test

--- lsy_drone_racing/control/trajectory_controller_challenge.py
+++ lsy_drone_racing/control/trajectory_controller_challenge.py
@@ -56,7 +56,7 @@ class TrajectoryController(Controller):
                 [0.5, -0.65, 1.1],
             ]
         )
-        self.t_total = 25
+        self.t_total = 24
         t = np.linspace(0, self.t_total, len(waypoints))
         self.trajectory = CubicSpline(
             t, waypoints, bc_type=((1, [0.0, 0.0, 0.5]), (2, [0.0, 0.0, 0.0]))
